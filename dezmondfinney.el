c2710e5115d910b5baf5d250cd83c24f69ff4640
Dezmond Finney <dezmondfinney@Dezmond-Finneys-MacBook.local>
2010-01-04 23:58:05 -0500

    Fixed org-mode agenda keybinding

--- dezmondfinney.el
+++ dezmondfinney.el
@@ -8,5 +8,5 @@
 ;; org-mode setup 
 (add-hook 'org-mode-hook 'variable-pitch-mode)
 (global-set-key "\C-cl" 'org-store-link)
-(global-set-key "C-ca" 'org-agenda)
+(global-set-key "\C-ca" 'org-agenda)
 (global-set-key "\C-cb" 'org-iswitchb)
